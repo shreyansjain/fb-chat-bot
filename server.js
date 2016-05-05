93bc14655eabfeaec5628c164f05237be1c96096
Shreyans Jain <jainshreyans@outlook.com>
2016-05-05 15:04:28 -0700

    added /webhook

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var http = require('http');
 var fs = require('fs');
 var app = express();
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'awesome_token') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
 app.get('/', function(req,res) {
     res.send('hello');
 });
