0cb91e3d60cb77f2ba944f1358a9fa50a539ff64
Shreyans Jain <jainshreyans@outlook.com>
2016-05-05 14:35:13 -0700

    use http server

--- server.js
+++ server.js
@@ -4,21 +4,12 @@ var http = require('http');
 var fs = require('fs');
 var app = express();
 
-var options = {
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.crt'),
-    ca: fs.readFileSync('ca.crt'),
-    requestCert: true,
-    rejectUnauthorized: false
-};
-
 app.get('/', function(req,res) {
     res.send('hello');
 });
 
-httpsPort = 4433;
-var httpsServer = https.createServer(options, app);
+httpPort = 8080;
 
-httpsServer.listen(httpsPort, function() {
-    console.log('Webserver started, Listening on port [' + httpsPort + ']');
+app.listen(httpPort, function() {
+    console.log('Webserver started, Listening on port [' + httpPort + ']');
 });
